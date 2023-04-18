69b705e1b2f4c3fe7afb790f1bad69b50e1a781f
Sathish Sk <67107485+sathishsksk@users.noreply.github.com>
2023-04-18 11:17:23 +0530

    Update helper.py

--- helper.py
+++ helper.py
@@ -2,25 +2,19 @@ import base64
 from pyDes import des, PAD_PKCS5, ECB
 import jiosaavn
 
-def format_song(data, lyrics):
+def format_song(data,lyrics):
     try:
         url = data['media_preview_url']
         url = url.replace("preview", "aac")
-        if data['320kbps'] == "true":
+        if data['320kbps']=="true":
             url = url.replace("_96_p.mp4", "_320.mp4")
         else:
             url = url.replace("_96_p.mp4", "_160.mp4")
         data['media_url'] = url
     except (KeyError, TypeError):
-        key = b"38346591"
-        des_cipher = des(key, ECB, padmode=PAD_PKCS5)
-        enc_url = base64.b64decode(data['encrypted_media_url'].strip())
-        dec_url = des_cipher.decrypt(enc_url)
-        dec_url = dec_url.decode('utf-8')
-        dec_url = dec_url.replace("_96.mp4", "_320.mp4")
-        if data['320kbps'] != "true":
-            dec_url = dec_url.replace("_320.mp4", "_160.mp4")
-        data['media_url'] = dec_url
+        data['media_url'] = decrypt_url(data['encrypted_media_url'])
+        if data['320kbps']!="true":
+            data['media_url'] = data['media_url'].replace("_320.mp4","_160.mp4")
 
     data['song'] = format(data['song'])
     data['music'] = format(data['music'])
@@ -42,7 +36,7 @@ def format_song(data, lyrics):
         pass
     return data
 
-def format_album(data, lyrics):
+def format_album(data,lyrics):
     data['image'] = data['image'].replace("150x150","500x500")
     data['name'] = format(data['name'])
     data['primary_artists'] = format(data['primary_artists'])
@@ -51,7 +45,7 @@ def format_album(data, lyrics):
         song = format_song(song,lyrics)
     return data
 
-def format_playlist(data, lyrics):
+def format_playlist(data,lyrics):
     data['firstname'] = format(data['firstname'])
     data['listname'] = format(data['listname'])
     for song in data['songs']:
@@ -60,3 +54,12 @@ def format_playlist(data, lyrics):
 
 def format(string):
     return string.replace("&quot;", "'").replace("&amp;", "&").replace("&#039;", "'")
+
+def decrypt_url(url):
+    key = b"38346591"
+    des_cipher = des(key, ECB, padmode=PAD_PKCS5)
+    enc_url = base64.urlsafe_b64decode(url + "==")
+    dec_url = des_cipher.decrypt(enc_url, padmode=PAD_PKCS5)
+    dec_url = dec_url.decode('utf-8')
+    dec_url = dec_url.replace("_96.mp4", "_320.mp4")
+    return dec_url
